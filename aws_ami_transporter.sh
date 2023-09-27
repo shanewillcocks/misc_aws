31b858cf4b45df077bc917d07e4f4eb311cf1cde
shanewillcocks <shane.willcocks@live.com>
2023-09-28 12:23:58 +1300

    Update aws_ami_transporter.sh

--- aws_ami_transporter.sh
+++ aws_ami_transporter.sh
@@ -5,13 +5,13 @@
 # Add a datestamp to the AMI prefix in Prod LZ for uniqueness
 datestamp=$(date '+%d%m%Y')
 # Product Name is the name we allocate to this instance of the provisioned AMI-Transporter
-product_name=AAP-RHEL8-AMI-Transporter-${datestamp}
+product_name=RHEL8-AMI-Transporter-${datestamp}
 dev_aws_id=$(get_octopusvariable "dev_aws_id")
 target_acct=$(get_octopusvariable "target_acct")
 key_arn=ebscmk_arn
 
 # Get the latest RHEL8 AMI ID
-rhel8_latest_ami=$(aws ec2 describe-images --owner ${dev_aws_id} --filters "Name=name,Values=BNZ-RHEL8-stable*" --query 'reverse(sort_by(Images, &CreationDate))[0]' | jq -r '.ImageId')
+rhel8_latest_ami=$(aws ec2 describe-images --owner ${dev_aws_id} --filters "Name=name,Values=*RHEL8-stable*" --query 'reverse(sort_by(Images, &CreationDate))[0]' | jq -r '.ImageId')
 # Get the Product ID from the AWS Service Catalog
 product_id=$(aws servicecatalog search-products --query "ProductViewSummaries[?Name=='AMI Transporter'].[ProductId]" --output text)
 # Get the latest Provisioning artefact ID (this is the version in the Service Catalog)
@@ -23,7 +23,7 @@ aws servicecatalog provision-product --provisioned-product-name ${product_name} 
 --provisioning-parameters Key=CopyPrefix,Value=AAP-${datestamp} Key=AMIId,Value=${rhel8_latest_ami} Key=TargetAccountId,Value=${target_acct} Key=SSMParameterName,Value=${key_arn}
 rc=$?
 if [ ${rc} -eq 0 ]; then
-  echo "AMI Transport to Prod successful"
+  echo "AMI Transport to Prod started successfully"
 else
-  echo "AMI Transprt to Prod failed: ${rc}"
+  echo "AMI Transport to Prod failed to start: ${rc}"
 fi
